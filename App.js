8547e3059a21ba710dae1f786bba24747538285e
Emerson Jean <marchimeson123@gmail.com>
2026-03-30 20:36:49 -0300

    teste emerson

--- App.js
+++ App.js
@@ -5,8 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
 
-      <Text>Teste Emer</Text>
-      <Text>Teste Emer</Text>
+      <Text>Alteração Emerson</Text>
       
       <StatusBar style="auto" />
     </View>
