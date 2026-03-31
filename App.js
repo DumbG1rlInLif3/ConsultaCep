4ab3016b462266dcb100147ad8726587e0346bd7
Jennifer Stefany <jennifer020605@edu.unipar.br>
2026-03-31 20:29:26 -0300

    alteração de style do bloco de consulta

--- App.js
+++ App.js
@@ -1,12 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View
+        style={{
+          flexDirection: "row",
+        }}
+      >
+        {/* Imagem da View via uri da web*/}
+        <Image
+          source={{
+            uri: "https://www.unipar.br/media/original_images/unipar_ld_schema_logo.png",
+          }}
+          style={{
+            width: 200,
+            // Largura da imagem
+            height: 150,
+            // Altura da imagem
+            marginTop: 30,
+            // Margem superior para afastar a imagem do topo da tela
+          }}
+        />
+      </View>
 
-      <Text>Criação do Projeto</Text>
-      
+      <View
+        style={{
+          alignItems: "center",
+          color: "#fff",
+          flex: 1,
+          backgroundColor: "#bd0b0b",
+          padding: 10,
+          borderRadius: 75,
+          width: 500,
+        }}
+      >
+        <Text
+          style={{
+            marginBottom: 50,
+            fontWeight: "bold",
+            fontSize: 30,
+            color: "#fff",
+          }}
+        >
+          CONSULTA CEP
+        </Text>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -15,8 +56,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
